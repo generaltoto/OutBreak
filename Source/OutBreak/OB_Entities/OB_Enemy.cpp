61f3513986916d995b716aef32f73cdac22b32b9
Thomas <thomasdelorme56@gmail.com>
2024-01-26 14:33:06 +0100

    🌟 ($Enemies) : Create first enemies base class

---
+++ Source/OutBreak/OB_Entities/OB_Enemy.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OB_Enemy.h"
+
+AOB_Enemy::AOB_Enemy()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AOB_Enemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AOB_Enemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+
---
+++ Source/OutBreak/OB_Entities/OB_Enemy.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "OB_Enemy.generated.h"
+
+UCLASS()
+class OUTBREAK_API AOB_Enemy : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	AOB_Enemy();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
